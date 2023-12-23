d44c491f636f60ca40ad8b8a77ba839e80885c2a
Fandoy <fandyglitch3@gmail.com>
2023-12-23 23:34:44 +0800

    csacsac

--- src/components/Layout/Drawer.tsx
+++ src/components/Layout/Drawer.tsx
@@ -52,6 +52,26 @@ const Drawer = (props: Props) => {
           className="drawer-overlay"
         ></label>
         <ul className="menu p-4 w-80 min-h-full bg-base-200">
+          <label
+            htmlFor="my-drawer-3"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          </label>
           <li>
             <a>Sidebar Item 1</a>
           </li>

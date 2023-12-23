27896b6aef593965852c826bbe8cae4ce7971c10
Fandoy <fandyglitch3@gmail.com>
2023-12-23 23:30:14 +0800

    cwcsa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
 export default function Page() {
-  return (
-    <div className="p-8 bg-black h-screen">
-      <div className="w-full h-24 bg-black text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-yellow-500">
-        {"Fandy Test Domain SEO Test"}
-      </div>
-    </div>
-  );
+  return <div className="p-8 bg-black h-screen">Hai Satrya</div>;
 }
